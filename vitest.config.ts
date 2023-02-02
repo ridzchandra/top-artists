7201e5cc20f2f7d02afeeed8110fdf745f0ad5dc
Ridz Balla <ridz@payble.com.au>
2023-02-02 14:34:36 +1100

    correcting packge.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,18 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     testTimeout: 30000,
   },
-  logLevel: "info",
+  logLevel: 'info',
   esbuild: {
-    sourcemap: "both",
+    sourcemap: 'both',
   },
   resolve: {
     alias: {
-      "@notes/core": "./services/core",
+      '@favourites/core': './services/core',
     },
   },
 });
